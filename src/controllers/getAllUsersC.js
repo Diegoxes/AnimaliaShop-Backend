610493aea076aed7cc659a6425f44e2b00fc8d16
SEmilianoLopez <emilopez197gmail.com>
2024-02-15 11:06:28 -0300

    correccion db

--- src/controllers/getAllUsersC.js
+++ src/controllers/getAllUsersC.js
@@ -5,21 +5,25 @@ const JSONFilePath = path.join(__dirname, "../../api/usuarios.json");
 
 const getAllUsersC = async (req) => {
   try {
-    const data = await fs.readFile(JSONFilePath, "utf-8"); // Using promises API for fs.readFile
-    const usersData = JSON.parse(data);
-    await Users.bulkCreate(usersData);
-    console.log('Usuarios precargados correctamente.');
+    // Verificar si la tabla Users está vacía
+    const count = await Users.count();
+    if (count === 0) {
+      const data = await fs.readFile(JSONFilePath, "utf-8"); // Using promises API for fs.readFile
+      const usersData = JSON.parse(data);
+      await Users.bulkCreate(usersData);
+      console.log('Usuarios precargados correctamente.');
+    }
 
     const users = await Users.findAll({
       order: [['email', 'ASC']], 
     });
 
     return users;
-  }  catch (error) {
+  } catch (error) {
     throw new Error(error.message);
   }
 }
 
 module.exports = {
-  getAllUsersC,
+  getAllUsersC,
 }
