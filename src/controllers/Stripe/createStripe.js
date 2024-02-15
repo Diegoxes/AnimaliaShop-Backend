da381608c3aa3a0c3e3b507c11cfa16d0bde91b4
SEmilianoLopez <emilopez197gmail.com>
2024-02-15 13:35:50 -0300

    correcciones2

--- src/controllers/Stripe/createStripe.js
+++ src/controllers/Stripe/createStripe.js
@@ -21,8 +21,8 @@ const createStripe = async (req, res) => {
     payment_method_types: ["card"],
     line_items: lineItems,
     mode: "payment",
-    success_url: "http://localhost:5173/success",
-    cancel_url: "http://localhost:5173/cancel",
+    success_url: "https://final-tau-virid.vercel.app/success",
+    cancel_url: "https://final-tau-virid.vercel.app/cancel",
   });
   res.json({ id: session.id });
 };
